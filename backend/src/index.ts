63984fd96f2f55882d805aeac304037a4eae8423
Nick <nsagias@gmail.com>
2023-05-11 19:47:54 -0400

    configure for standalone to work

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
 import { expressMiddleware } from "@apollo/server/express4";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
@@ -11,6 +12,44 @@ import compress from "compression";
 
 const app = express();
 
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = async () => {
+  const { url } = await startStandaloneServer(server, { listen: { port: 4000 }, });
+  console.log(`🚀  Server ready at: ${url} localhost:4000`);
+} ;
+
+startServer();
 
 
-app.listen(8000, () => console.log("Listening on port http://localhost:8000"));
+// app.listen(8000, () => console.log("Listening on port http://localhost:8000"));
